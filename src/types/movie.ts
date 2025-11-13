0637462e92cc17676bae9b52520c93979f97d0f2
Luiz Gustavo <contatoluizgustavoig@gmail.com>
2025-11-13 20:25:50 -0300

    feat: Movie Type

---
+++ src/types/movie.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  popularity: number;
+  revenue?: number;
+  genres?: { id: number; name: string }[];
+}
