9e30d55dd4709e8bcea19b1ae54349878b5a4166
Luiz Gustavo <contatoluizgustavoig@gmail.com>
2025-11-13 20:58:54 -0300

    feat: improving movie types

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,12 +1,24 @@
+export interface Genre {
+  id: number;
+  name: string;
+}
+
 export interface Movie {
   id: number;
   title: string;
-  overview: string;
-  poster_path: string | null;
-  release_date: string;
-  vote_average: number;
-  vote_count: number;
-  popularity: number;
+  overview?: string;
+  poster_path?: string | null;
+  release_date?: string;
+  vote_average?: number;
+  vote_count?: number;
+  popularity?: number;
   revenue?: number;
-  genres?: { id: number; name: string }[];
+  genres?: Genre[];
 }
+
+export type PaginatedResponse<T> = {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+};
